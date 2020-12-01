2fd555e1871daf088bc8419f73165aa6a03890f9
Marcus Backholm <marcus.backholm@live.se>
2020-12-01 17:56:15 +0100

    Lagt till tester i Context.cs för Tables-datan

--- DataBaseConnection/Context.cs
+++ DataBaseConnection/Context.cs
@@ -24,24 +24,28 @@ namespace DataBaseConnection
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            Customer[] customers =
+            modelBuilder.Entity<Customer>().HasData(new Customer
             {
-                    new Customer { Firstname = "Alan", Lastname = "Weik", Age = 12, Email = "AlanWeik@gmail.com" },
-                    new Customer { Firstname = "Svante", Lastname = "Pålsson", Age = 66, Email = "SvantePålsson@gmail.com" },
-                    new Customer { Firstname = "Marcus", Lastname = "Häst", Age = 45, Email = "Marcus@gmail.com" },
-                    new Customer { Firstname = "Peter", Lastname = "Svensson", Age = 17, Email = "PeterS@gmail.com" },
-                    new Customer { Firstname = "Tony", Lastname = "Makaroni", Age = 19, Email = "Tony@gmail.com" },
-                    new Customer { Firstname = "Gustav", Lastname = "Marklund", Age = 20, Email = "Gurra@gmail.com" },
-                    new Customer { Firstname = "Mark", Lastname = "Gustavsson", Age = 20, Email = "MarkG@gmail.com" },
-                    new Customer { Firstname = "Lisa", Lastname = "Field", Age = 12, Email = "LisaF@gmail.com" },
-                    new Customer { Firstname = "Alma", Lastname = "Svensson", Age = 45, Email = "Alma@gmail.com" },
-                    new Customer { Firstname = "Fanny", Lastname = "Wihlborg", Age = 33, Email = "Fanny@gmail.com" },
-                    new Customer { Firstname = "Lina", Lastname = "Lindberg", Age = 66, Email = "Lina@gmail.com" },
-                    new Customer { Firstname = "Stina", Lastname = "Elvland", Age = 99, Email = "Stina@hotmail.com" },
-                    new Customer { Firstname = "Alice", Lastname = "Eklund", Age = 32, Email = "Alice@hotmail.com" },
-                    new Customer { Firstname = "Saga", Lastname = "Eklund", Age = 11, Email = "Saga@hotmail.com" },
-            };
+                Id = 1,
+                Firstname = "Alan",
+                Lastname = "Weik",
+                Age = 12,
+                Email = "AlanWeik@gmail.com"
+            });
 
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 1,
+                Title = "Lord of the rings ",
+                Genre = "Fantasy",
+                ImageURL = ""
+            });
+
+            modelBuilder.Entity<Rental>().HasData(new Rental
+            {
+                Id = 1,
+                Date = DateTime.Now
+            });
         }
     }
 }
