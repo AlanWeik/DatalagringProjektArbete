ae75461974db34ba4197cb2337edf1806468e9a3
Marcus Backholm <marcus.backholm@live.se>
2020-12-01 16:28:29 +0100

    Redigerade om så att förnamn och efternamn är separata samt ålder tillagt i models.cs

--- DataBaseConnection/Seeding.cs
+++ DataBaseConnection/Seeding.cs
@@ -17,20 +17,20 @@ namespace DataBaseConnection
                 ctx.RemoveRange(ctx.Customers);
 
                 ctx.AddRange(new List<Customer> {
-                    new Customer { Name = "Alan Weik", Email = "AlanWeik@gmail.com" },
-                    new Customer { Name = "Svante Pålsson", Email = "SvantePålsson@gmail.com" },
-                    new Customer { Name = "Marcus Häst", Email = "Marcus@gmail.com" },
-                    new Customer { Name = "Peter Svensson", Email = "PeterS@gmail.com" }, 
-                    new Customer { Name = "Tony Makaroni", Email = "Tony@gmail.com" },
-                    new Customer { Name = "Gustav Marklund", Email = "Gurra@gmail.com" },
-                    new Customer { Name = "Mark Gustavsson", Email = "MarkG@gmail.com" },
-                    new Customer { Name = "Lisa Field", Email = "LisaF@gmail.com" }, 
-                    new Customer { Name = "Alma Svensson", Email = "Alma@gmail.com" },
-                    new Customer { Name = "Fanny Wihlborg", Email = "Fanny@gmail.com" },
-                    new Customer { Name = "Lina Lindberg", Email = "Lina@gmail.com" }, 
-                    new Customer { Name = "Stina Elvland", Email = "Stina@hotmail.com" }, 
-                    new Customer { Name = "Alice Eklund", Email = "Alice@hotmail.com" },
-                    new Customer { Name = "Saga Eklund", Email = "Saga@hotmail.com" },
+                    new Customer { Firstname = "Alan", Lastname = "Weik", Age = 12, Email = "AlanWeik@gmail.com" },
+                    new Customer { Firstname = "Svante", Lastname = "Pålsson", Age = 66, Email = "SvantePålsson@gmail.com" },
+                    new Customer { Firstname = "Marcus", Lastname = "Häst", Age = 45, Email = "Marcus@gmail.com" },
+                    new Customer { Firstname = "Peter", Lastname = "Svensson", Age = 17, Email = "PeterS@gmail.com" },
+                    new Customer { Firstname = "Tony", Lastname = "Makaroni", Age = 19, Email = "Tony@gmail.com" },
+                    new Customer { Firstname = "Gustav", Lastname = "Marklund", Age = 20, Email = "Gurra@gmail.com" },
+                    new Customer { Firstname = "Mark", Lastname = "Gustavsson", Age = 20, Email = "MarkG@gmail.com" },
+                    new Customer { Firstname = "Lisa", Lastname = "Field", Age = 12, Email = "LisaF@gmail.com" },
+                    new Customer { Firstname = "Alma", Lastname = "Svensson", Age = 45, Email = "Alma@gmail.com" },
+                    new Customer { Firstname = "Fanny", Lastname = "Wihlborg", Age = 33, Email = "Fanny@gmail.com" },
+                    new Customer { Firstname = "Lina", Lastname = "Lindberg", Age = 66, Email = "Lina@gmail.com" },
+                    new Customer { Firstname = "Stina", Lastname = "Elvland", Age = 99, Email = "Stina@hotmail.com" },
+                    new Customer { Firstname = "Alice", Lastname = "Eklund", Age = 32, Email = "Alice@hotmail.com" },
+                    new Customer { Firstname = "Saga", Lastname = "Eklund", Age = 11, Email = "Saga@hotmail.com" },
 
                 });
 
